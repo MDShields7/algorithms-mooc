626e8c3d898a2eb7262d0e516301d366d182733b
MDShields7 <matthewshields@Matthews-MacBook-Pro-V20.local>
2020-04-03 17:05:31 -0500

    files added

---
+++ Week1/QuickUnionUF.java
@@ -0,0 +1,21 @@
+public class QuickUnionUF  {
+    private int[] id;
+    
+    public QuickUnionUF(int N) {
+        id = new int[N];
+        for ( int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+    
+    private int root ( int i ) {
+        while ( i != id[i] ) {
+            i = id[i];
+        }
+    return i;
+    }
+    
+    public boolean connected ( int p, int q ) {
+        return root(p) == root(q);
+  }
+}
