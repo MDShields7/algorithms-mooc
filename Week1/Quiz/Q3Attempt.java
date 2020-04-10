1b3f28d84981f26c7d37963e920eba1ddce6ecbe
MDShields7 <matthew.duncan.shields@gmail.com>
2020-04-09 23:52:23 -0500

    question 3 finished

--- Week1/Quiz/Q3Attempt.java
+++ Week1/Quiz/Q3Attempt.java
@@ -1,55 +1,79 @@
 
 public class Q3Attempt {
-    private int[] id;
+    private int nextRoot;
+    private int prevRoot;
+    // n equals root index
+
+    private int[] next;
     private int[] prev;
+    // n equals x + n (1 equals x + 1)
+    // -1 equals removed
+    // 0 equals self
 
     public Q3Attempt(int N) {
-        id = new int[N];
+        nextRoot = N-1;
+        prevRoot = 0;
         for ( int i = 0; i < N; i++ ) {
             if ( i == N ) {
-                id[i] = i;
+                next[i] = 0;
             }
             if ( i < N ) {
-                id[i] = i + 1;
+                next[i] = 1;
             }
             if ( i == 0 ) {
-                prev[i] = i;
+                prev[i] = 0;
             }
             if ( i > N ) {
-                prev[i] = i - 1;
+                prev[i] = 1;
             }
         }
     }
 
-    // How do arrays or lists work in java?...
+    // Array indexes are marked -1 on removal
     public void remove(int x) {
-        int xMinus = x - 1;
-        int xPlus = x + 1;
-        if ( id[x] == null) {
+        if ( next[x] == -1 || prev[x] == -1 ) {
             return;
+        } else if ( prevRoot == nextRoot ) {
+            return;
+        } else if ( next[x] == nextRoot ) {
+            // the last successor item in list
+            next[ x - prev[x] ] = 0;
+            next[x] = -1;
+        } else if ( prev[x] == prevRoot ) {
+            // the last successor item in list
+            prev[ x + next[x] ] = 0;
+            prev[x] = -1;
+        } else {
+            int nextX = next[x];
+            int prevX = prev[x];
+            next[ x - prevX ] += nextX;
+            prev[ x + nextX ] += prevX;
+            next[x] = -1;
+            prev[x] = -1;
         }
-        if ( id[x] == x ) {
-            id[xMinus] = xMinus;
-        }
-        if ( prev[x] == 0 ) {
-            prev[xPlus] = xPlus;
-        }
-        
+        prevRoot = prevRoot + next[x];
+        nextRoot = nextRoot - prev[x];
     }
-
-    private int next(int x) {
-        return id[x];
-    }
-
-    // private int root ( int i ) {
-    //     while ( i != id[i] ) {
-    //         // id[i] = id[id[i]];
-    //         i = id[i];
-    //     }
-    //     return i;
-    // }
-
-    // public void union( int p, int q ){
-    
-    // }
 }
+
+// EXAMPLE
+// S =   { 0, 1, 2, 3}
+// next    1, 1, 1, 0
+// prev    0, 1, 1, 1
+// nextroot = 3
+// prevroot = 0
+//   REMOVE S[1]
+// nextX = 1, prevX = 1
+// S =   { 0, 1, 2, 3}
+// next    2,-1, 1, 0
+// prev    0,-1, 2, 1
+//   REMOVE S[2]
+// S =   { 0, 1, 2, 3}
+// nextX = 1, prevX = 2
+// next    3,-1,-1, 0
+// prev    0,-1,-1, 3   
+//   REMOVE S[3]
+// S =   { 0, 1, 2, 3}
+// nextX = 0, prevX = 3
+// next    3,-1,-1, 0
+// prev    0,-1,-1, 3  
